44e4de91a5c8abf43ebbaf0340fa1968807dd5e1
Mike Laurel <mlaurel@hey.com>
2025-03-07 12:08:31 -0800

    Staging changes before switching to step/5

--- src/agent.ts
+++ src/agent.ts
@@ -18,15 +18,16 @@ export const runAgent = async ({
 
     const response = await runLLM({ messages: history, tools })
     await addMessages([response])
-
+    
+    
     if (response.tool_calls) {
         const toolCall = response.tool_calls[0]
         const toolResponse = await runTool(toolCall, userMessage)
         // console.log(`toolResponse is: ${toolResponse}`);
         await saveToolResponse(toolCall.id, toolResponse)
-        loader.update(`done: ${toolCall.function.name}`)
+        loader.update(`executing: ${toolCall.function.name}`)
     }
-
+    
     logMessage(response)
     loader.stop()
     return getMessages()
